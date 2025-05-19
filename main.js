85d58abffe83c91015850e6fe2e6def6b4f2e5ad
Kisváradi Márton (Szf_N_2024_2026) <kisvaradi.marton@diak.szamalk-szalezi.hu>
2025-05-19 13:27:52 +0200

    asdasd

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 import Kartyak from "./Kartyak.js";
 import { konnyuLista } from "./konnyuLista.js";
+import { nehezLista } from "./nehezLista.js";
 
+let konnyuGomb = document.querySelector("#konnyuGomb")
+let nehezGomb = document.querySelector("#nehezGomb")
 let pElem = document.querySelector(".kartya")
 
-new Kartyak(pElem, konnyuLista)
+console.log(konnyuGomb)
+konnyuGomb.addEventListener("click", () => {
+    new Kartyak(pElem, konnyuLista)
+    konnyuGomb.style.display = "none"
+    nehezGomb.style.display = "none"
+})
+
+nehezGomb.addEventListener("click", () => {
+    new Kartyak(pElem, nehezLista)
+    konnyuGomb.style.display = "none"
+    nehezGomb.style.display = "none"
+})
---
+++ nehezLista.js
@@ -0,0 +1,32 @@
+export const nehezLista = [
+    {
+        kerdes: "Mi volt a Magna Charta kiadásának éve?",
+        valaszok: ["1215", "1066", "1492", "1789"],
+        helyesek: [0],
+        magyarazat: "A Magna Charta 1215-ben készült Angliában."
+    },
+    {
+        kerdes: "Melyik ország nyerte a legtöbb labdarúgó-világbajnokságot (2022-ig)?",
+        valaszok: ["Németország", "Argentína", "Brazília", "Olaszország"],
+        helyesek: [2],
+        magyarazat: "Brazília 5 világbajnoki címmel a legsikeresebb."
+    },
+    {
+        kerdes: "Mi Schrödinger híres gondolatkísérlete?",
+        valaszok: ["A végtelen majomtétele", "A Schrödinger macskája", "A kettős rés kísérlet", "Heisenberg határozatlansága"],
+        helyesek: [1],
+        magyarazat: "A Schrödinger macskája a kvantummechanika paradoxonaira utal."
+    },
+    {
+        kerdes: "Ki írta a 'Tóték' című drámát?",
+        valaszok: ["Móricz Zsigmond", "Kosztolányi Dezső", "Örkény István", "Nádas Péter"],
+        helyesek: [2],
+        magyarazat: "Örkény István egyperces novelláiról és a Tóték című drámájáról híres."
+    },
+    {
+        kerdes: "Melyik szerv termel inzulint?",
+        valaszok: ["Máj", "Hasnyálmirigy", "Gyomor", "Vese"],
+        helyesek: [1],
+        magyarazat: "Az inzulint a hasnyálmirigy termeli."
+    }
+]
